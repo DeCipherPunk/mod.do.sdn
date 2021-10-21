a6dea071113d2669f0f4234ccacd7851ca1c0d39
DeCipherPunk <decipherpunk@protonmail.com>
2021-10-21 15:07:17 +0530

    fix typoes

--- variables.tf
+++ variables.tf
@@ -1,21 +1,21 @@
 variable "do_token" {}
 variable "sdn_name"{
-    decription = "Name of the SDN Network"
+    description = "Name of the SDN Network"
     type = string
 }
 
 variable "sdn_region"{
-    decription = "Region for the SDN Network"
+    description = "Region for the SDN Network"
     type = string
 }
 
 
 variable "sdn_cidr_block_ipv4"{
-    decription = "CIDR IPv4 Block for the SDN Network"
+    description = "CIDR IPv4 Block for the SDN Network"
     type = string
 }
 
 variable "stack_tags"{
-    decription = "CIDR IPv4 Block for the SDN Network"
+    description = "CIDR IPv4 Block for the SDN Network"
     type = list
 }
