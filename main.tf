c4284ffe9523d2be7b674f6ae02ef52cbdf8148b
DeCipherPunk <decipherpunk@protonmail.com>
2021-10-21 15:19:25 +0530

    remove unsupported tags from sdn for DO

--- main.tf
+++ main.tf
@@ -13,5 +13,4 @@ resource "digitalocean_vpc" "sdn" {
   name     = "${var.sdn_name}-sdn"
   region   = "${var.sdn_region}"
   ip_range = "${var.sdn_cidr_block_ipv4}"
-  tags = "${var.stack_tags}"
 }
